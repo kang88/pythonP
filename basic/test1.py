71990342c8810e6770c32aaca7da59e30d2f9de7
kang88 <kso8868@naver.com>
2019-09-02 12:52:18 +0900

    20190902 두번째 커밋

--- basic/test1.py
+++ basic/test1.py
@@ -1,1 +1,25 @@
 print("hi~")
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+s='python'
+s1="python"
+s2="python's"
+s3='"python"'
+s4='''python'''
+s5="""python"""
+s6="p y"
+s7='''p
+      y
+      '''
+
+
+head="7678"
+tail=" is fun!"
+print(head+tail)
+print(type(head))
+print(type(tail))
